d185a921cc4bd33294f13023703483eba8325a5b
d-445 <dhyanpik@gmail.com>
2025-06-12 14:59:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function playNow() {
-const select = document.getElementById('gameSelect');
-const url = select.value;
+    const select = document.getElementById('gameSelect');
+    const url = select.value;
 
-if (url) {
-window.location.href = url;
-} else {
-alert('Please select a version/beta version first!');
-}
+    if (url) {
+        window.location.href = url;
+    } else {
+        alert('Please select a version/beta version first!');
+    }
 }
