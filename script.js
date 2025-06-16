0df8bf219d107bad3fb9335ba6119c257f5cf2c6
d-445 <dhyanpik@gmail.com>
2025-06-16 10:54:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-// Mobile menu toggle
-document.addEventListener('DOMContentLoaded', function() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var nav = document.querySelector('.header-right');
-    if(menuToggle && nav) {
-        menuToggle.addEventListener('click', function() {
-            nav.classList.toggle('show');
-        });
-        // Hide menu when a link is clicked (on mobile)
-        nav.querySelectorAll('a').forEach(function(link) {
-            link.addEventListener('click', function() {
-                nav.classList.remove('show');
-            });
-        });
+function playNow() {
+    const select = document.getElementById('gameSelect');
+    const url = select.value;
+
+    if (url) {
+        window.location.href = url;
+    } else {
+        alert('Please select a version/beta version first!');
     }
-});
+}
