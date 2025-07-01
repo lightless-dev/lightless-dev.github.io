30fdf590ff3c5991310d96b6198042caec0eb861
d-445 <dhyanpik@gmail.com>
2025-07-01 09:36:53 -0400

    Create gamescript.js

---
+++ gamescript.js
@@ -0,0 +1,4 @@
+window.addEventListener('resize', function() {
+  document.querySelector('iframe').style.height = window.innerHeight + 'px';
+});
+document.querySelector('iframe').style.height = window.innerHeight + 'px';
